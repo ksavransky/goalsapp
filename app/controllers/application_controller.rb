a253b67dd1b8c3aa49903afc1a57300636a9cb43
App Academy Student <student@appacademy.io>
2016-08-08 11:59:29 -0700

    Phase 1 complete - auth done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,34 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include ApplicationHelper
+
+  helper_method :current_user
+  helper_method :signed_in?
+
+
+  def sign_in(user)
+    @current_user = user
+    session[:session_token] = @current_user.reset_token
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def sign_out
+    current_user.try(:reset_token)
+    session[:session_token] = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def require_token
+    redirect_to root_url unless signed_in?
+  end
+
+
 end
